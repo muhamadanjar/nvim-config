77c691015f9c3c41bb933508d9b5258d322bff4b
Muhamad Anjar <arvanria@gmail.com>
2022-08-21 15:13:22 +0700

    add base lua config

---
+++ lua/highlights.lua
@@ -0,0 +1,7 @@
+print("loaded highlight")
+vim.opt.cursorline = true
+vim.opt.termguicolors = true
+vim.opt.winblend = 0
+vim.opt.wildoptions = 'pum'
+vim.opt.pumblend = 5
+vim.opt.background = 'dark'
---
+++ lua/maps.lua
@@ -0,0 +1,8 @@
+local keymap = vim.keymap
+
+keymap.set("n", "x", '"_x')
+
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+
