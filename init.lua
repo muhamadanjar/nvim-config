daaf1f362c0d973e972269d1eae606282c68a1c4
Muhamad Anjar <arvanria@gmail.com>
2022-08-22 11:12:35 +0700

    remove unused

--- init.lua
+++ init.lua
@@ -2,3 +2,5 @@ require("base")
 require("highlights")
 require("maps")
 require("plugins")
+
+vim.opt.clipboard:append { 'unnamedplus' }
--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -1,23 +1,23 @@
-local status, lsp = pcall(require, 'lspconfig')
+local status, nvim_lsp = pcall(require, 'lspconfig')
 if (not status) then return end
 
-local protocol = require("nvim.lsp.protocol")
-local on_attach =  function(client, bufnr)
+local protocol = require("vim.lsp.protocol")
+local on_attach = function(client, bufnr)
   if client.server_capabilities.documentFormattingProvider then
-    vim.api.nvim_command [[autogroup Format]]
+    vim.api.nvim_command [[augroup Format]]
     vim.api.nvim_command [[autocmd! * <buffer>]]
-    vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format]]
-    vim.api.nvim_command [[autogroup END]]
+    vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
+    vim.api.nvim_command [[augroup END]]
   end
 end
-lsp.tsserver.setup {
-  on_attach = on_attach,
-  filetypes = {"typescript", "typescriptreact", "typescript.tsx" },
-  cmd = { "typescript-language-server", "--stdio" },
 
+nvim_lsp.tsserver.setup {
+  on_attach = on_attach,
+  filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+  cmd = { "typescript-language-server", "--stdio" },
 }
 
-lsp.sumneko_lua.setup {
+nvim_lsp.sumneko_lua.setup {
   on_attach = on_attach,
   settings = {
     Lua = {
